bf9a535794cc1f54f9618be5009e020c52dffc75
kai10107 <kaiaustin@icloud.com>
2023-12-31 14:03:41 +1100

    adds javascript code for left and right side of page

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const h2 = document.createElement("h2");
-h2.textContent = "this content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.container');
+
+left.addEventListener('mouseenter', () => {
+  container.classList.add('hover-left');
+});
+
+left.addEventListener('mouseleave', () => {
+  container.classList.remove('hover-left');
+});
+
+right.addEventListener('mouseenter', () => {
+  container.classList.add('hover-right');
+});
+
+right.addEventListener('mouseleave', () => {
+  container.classList.remove('hover-right');
+});
