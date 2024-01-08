884d5f1a8ea7415ad94c63d7f35298a4c3e6677e
kai10107 <kaiaustin@icloud.com>
2024-01-08 12:29:10 +1100

    adds a counter to the guess

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+let attempts = 0;
+
 document.getElementById("guess-form").onsubmit = function(event) {
   event.preventDefault();
+
+  attempts++;
 
   const actualMarbles = 407;
   let userGuess = document.getElementById("guess").value;
   userGuess = parseInt(userGuess);
 
   
-  let resultText = `You guessed ${userGuess}. `;
+  let resultText = `Attempt ${attempts}: You guessed ${userGuess}. `;
   if (userGuess === actualMarbles) {
-      resultText += "That's exactly right!";
+      resultText += "That's exactly right! You found the correct number in ${attempts}: attempts. ";
+            attempts = 0; 
   } else if (userGuess > actualMarbles) {
       resultText += "That's too high. Try guessing lower.";
   } else {
