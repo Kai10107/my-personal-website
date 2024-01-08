9e5faa7df3d22e49803ece6ffc40af75ca0d5423
kai10107 <kaiaustin@icloud.com>
2024-01-08 14:46:26 +1100

    adds a transition effect which fades in the results area

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ document.getElementById("guess-form").onsubmit = function(event) {
   event.preventDefault();
 
   attempts++;
-
   const actualMarbles = 407;
   let userGuess = document.getElementById("guess").value;
   userGuess = parseInt(userGuess);
@@ -13,15 +12,21 @@ document.getElementById("guess-form").onsubmit = function(event) {
   let resultText = ` You guessed ${userGuess}. `;
   if (userGuess === actualMarbles) {
       resultText += "That's exactly right! You found the correct number in ${attempts}: attempts. ";
-             
+
+      const resultsSection = document.getElementById("results-section");
+      resultsSection.style.display = 'block';
+      setTimeout(() => resultsSection.style.opacity = 1, 10); 
+
+
+
+
+document.getElementById("game-results").innerText = `Congratulations!`;
   } else if (userGuess > actualMarbles) {
       resultText += "That's too high. Try guessing lower.";
   } else {
       resultText += "That's too low. Try guessing higher.";
   }
+
   document.getElementById("attempts-display").innerText = `Attempts: ${attempts}`;
-
-
-
   document.getElementById("result").innerText = resultText;
 };
